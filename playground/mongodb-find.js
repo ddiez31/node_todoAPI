54c66a8addb9f6dbbef3a92f473a28edf0460ece
ldevernay <boblehamster@yahoo.fr>
2017-07-31 16:49:14 +0200

    mongodb find users

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -24,13 +24,21 @@ MongoClient.connect('mongodb://localhost:27017/TodoAPI', (err, db) => {
   // });
 
 
-  db.collection('Todos').find({
-    _id: new ObjectID('597f37d2a2e9788e3fef72b9')
-  }).count().then((count) => {
-    console.log(`Todos count: ${count}`);
-  }, (err) => {
-    console.log('Unable to fetch todos', err)
-  });
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('597f37d2a2e9788e3fef72b9')
+  // }).count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err)
+  // });
 
+  db.collection('Users').find({
+    name: 'Mike'
+  }).toArray().then((docs) => {
+    console.log(`Users: ${JSON.stringify(docs, undefined, 2)}`);
+  }, (err) => {
+    console.log('Unable to fetch users', err)
+  });
+  //
   // db.close();
 })
