09e7b62bc5c5c609ac86c104fca92de47d1bd07a
ldevernay <boblehamster@yahoo.fr>
2017-07-31 15:11:43 +0200

    mongodb user insertion

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -6,12 +6,23 @@ MongoClient.connect('mongodb://localhost:27017/TodoAPI', (err, db) => {
   }
   console.log('Connected to MongoDB server');
 
-  db.collection('Todos').insertOne({
-    name: 'first thing to do',
-    completed: false
+  // db.collection('Todos').insertOne({
+  //   name: 'first thing to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if(err){
+  //     return console.log('Unable to insert Todo', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+  //
+  db.collection('Users').insertOne({
+    name: 'Bob',
+    age: 34,
+    location: 'StGo'
   }, (err, result) => {
     if(err){
-      return console.log('Unable to insert Todo', err);
+      return console.log('Unable to insert User', err);
     }
     console.log(JSON.stringify(result.ops, undefined, 2));
   });
