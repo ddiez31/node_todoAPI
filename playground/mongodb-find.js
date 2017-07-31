0608e062a03b7b7a5b24a9d072323200222a0d7c
ldevernay <boblehamster@yahoo.fr>
2017-07-31 16:30:54 +0200

    mongodb count todos

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -9,11 +9,28 @@ MongoClient.connect('mongodb://localhost:27017/TodoAPI', (err, db) => {
   }
   console.log('Connected to MongoDB server');
 
-  db.collection('Todos').find().toArray().then((docs) => {
-    console.log(`Todos: ${JSON.stringify(docs, undefined, 2)}`);
+  // db.collection('Todos').find().toArray().then((docs) => {
+  //   console.log(`Todos: ${JSON.stringify(docs, undefined, 2)}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err)
+  // });
+  //
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('597f37d2a2e9788e3fef72b9')
+  // }).toArray().then((docs) => {
+  //   console.log(`Todos: ${JSON.stringify(docs, undefined, 2)}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err)
+  // });
+
+
+  db.collection('Todos').find({
+    _id: new ObjectID('597f37d2a2e9788e3fef72b9')
+  }).count().then((count) => {
+    console.log(`Todos count: ${count}`);
   }, (err) => {
     console.log('Unable to fetch todos', err)
   });
 
-  db.close();
+  // db.close();
 })
