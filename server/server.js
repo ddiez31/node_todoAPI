6b61d97a69607b96fb211e07bd8991be75b23aef
ldevernay <boblehamster@yahoo.fr>
2017-08-02 12:29:31 +0200

    init mongoose

---
+++ server/server.js
@@ -0,0 +1,30 @@
+let mongoose = require('mongoose');
+
+//mongoose promises handled by core JS, not by a third-party library
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/TodoAPI');
+
+
+let Todo = mongoose.model('Todo', {
+  text: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  },
+  completedAt: {
+    type: Number
+  }
+});
+
+let newTodo = new Todo({
+  text: 'git init',
+  completed: true
+});
+
+newTodo.save().then((doc) => {
+  console.log('Saved todo', JSON.stringify(doc, undefined, 2));
+}, (err) => {
+  console.log('Unable to save todo', err);
+});
