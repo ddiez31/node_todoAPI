5b0b87266873117d329936727f96ff0c3c10d15d
ldevernay <boblehamster@yahoo.fr>
2017-08-02 15:52:07 +0200

    playground : user queries

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,17 +1,39 @@
-const {mongoose} = require('./../server/db/mongoose');
-const {Todo} = require('./../server/models/todo');
 const {ObjectID} = require('mongodb');
 
-let id = "5981d45ced02332951f5df10";
 
-Todo.find({
-  _id: new ObjectID(id)
-}).then((todos) => {
-  console.log('Todos: ', todos);
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+
+// ===================TODOS====================
+let id = "qgfzqrgqrz";
+
+if (!ObjectID.isValid(id)){
+  console.log('Invalid ID.');
+}
+
+Todo.findById(id).then((todo) => {
+  if(!todo){
+    return console.log('Id not found.');
+  }
+  console.log('Todo by id: ', todo);
+}).catch((e) => {
+  console.log(e);
 });
 
-Todo.findOne({
-  _id: id
-}).then((todo) => {
-  console.log('Todo: ', todo);
+// ===================USERS====================
+let user_id = "qgzzqg";
+
+if(!ObjectID.isValid(user_id)){
+  return console.log('Invalid user ID.');
+}
+
+User.findById(user_id).then((user) => {
+  if(!user){
+    return console.log('User not found');
+  }
+  console.log('User by ID: ', user);
+}).catch((e) => {
+  console.log(e);
 });
