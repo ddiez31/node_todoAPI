50a18095793c6295ffedbc41399d3c2cbc0037d7
ldevernay <boblehamster@yahoo.fr>
2017-08-03 09:59:54 +0200

    connect to remote MongoDB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ let mongoose = require('mongoose');
 //mongoose promises handled by core JS, not by a third-party library
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/TodoAPI');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoAPI');
 
 module.exports = {
   mongoose
