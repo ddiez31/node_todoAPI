a954b5ea0661568d1253b68d56c80c538f0abbe7
ldevernay <boblehamster@yahoo.fr>
2017-08-03 10:29:52 +0200

    playground : remove todos

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,18 @@
+const {ObjectID} = require('mongodb');
+
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Remove all todos
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+// Remove a Todo
+Todo.findOneAndRemove({
+  text: 'first test todo'
+}).then((todo) => {
+  console.log(JSON.stringify(todo, undefined, 2));
+});
